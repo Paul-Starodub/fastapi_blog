cd07505c5d5bf5eabb7536f087e16e7609a4c20e
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-28 22:39:49 +0200

    add templates & static

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 posts: list[dict] = [
     {
@@ -25,10 +27,16 @@ posts: list[dict] = [
 ]
 
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts/", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+# @app.get("/", response_class=HTMLResponse, include_in_schema=False)
+# @app.get("/posts/", response_class=HTMLResponse, include_in_schema=False)
+# def home():
+#     return f"<h1>{posts[0]['title']}</h1>"
+
+
+@app.get("/", include_in_schema=False)
+@app.get("/posts/", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts})
 
 
 @app.get("/api/posts/")
