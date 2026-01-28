105f03019807b09b9797593bc9452e1cf4efbe28
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-28 23:38:11 +0200

    add endpoint for detail post retrieval

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -44,3 +44,11 @@ def home(request: Request):
 @app.get("/api/posts/")
 def get_posts() -> list[dict]:
     return posts
+
+
+@app.get("/api/posts/{post_id}/")
+def get_posts(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
