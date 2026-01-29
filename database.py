c1f97f5725ab3f9fa3ba2b902b802b96b4686cce
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-29 23:52:05 +0200

    update requirements for async sqlite support and database.py

--- database.py
+++ database.py
@@ -1,20 +1,42 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+######## async version sqlite ##########
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
+
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+######## sync version sqlite ##########
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"check_same_thread": False},
+# )
+#
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+# def get_db():
+#     with SessionLocal() as db:
+#         yield db
+#
